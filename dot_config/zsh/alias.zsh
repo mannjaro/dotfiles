4103374f690a05d59347363ea6c03a379e2feabd
Takayuki Zukawa <zk.takayuki@gmail.com>
2022-07-02 10:50:12 +0900

    zshrc -> .config/zsh

---
+++ dot_config/zsh/alias.zsh
@@ -0,0 +1,8 @@
+alias ls='ls -GF'
+alias ll='ls -lGF'
+alias la='ls -alGF'
+
+alias ..="cd ../"
+alias ...="cd ../../"
+alias ../="cd ../"
+alias .../="cd ../../"
---
+++ dot_config/zsh/style.zsh
@@ -0,0 +1,2 @@
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*:default' menu select=1
