d38962f7391c1f5a243fd46854aa2cb59ce4c316
Takayuki Zukawa <zk.takayuki@gmail.com>
2022-07-02 11:15:35 +0900

    Update dot_config/zsh/sync.zsh

--- dot_config/zsh/sync.zsh
+++ dot_config/zsh/sync.zsh
@@ -1,2 +1,10 @@
 eval "$(starship init zsh)"
+
 setopt auto_cd
+setopt auto_menu
+setopt correct
+setopt magic_equal_subst
+setopt nobeep
+setopt nolistbeep
+setopt print_eight_bit
+setopt share_history
