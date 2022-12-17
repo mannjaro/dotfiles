57b2ec2daa82d8c764d2f32c6a074bc9d93cff86
Takayuki Zukawa <zk.takayuki@gmail.com>
2022-12-17 10:16:10 +0900

    Update .config/fish/config.fish Add .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,9 +3,12 @@ if status is-interactive
 end
 starship init fish | source
 
-
 abbr -a cat bat
-abbr -a ls exa
+abbr -a ls exa --icons
+abbr -a la exa --icons -haF
+abbr -a ll exa --icons -halF --git
+abbr -a lt exa --icons --tree --level=3
 abbr -a grep rg
 abbr -a find fd
 abbr -a vim nvim
+
