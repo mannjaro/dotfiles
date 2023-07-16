80204cc943e67d15aa3061b2d0ac5ba4e47b955f
mandarium <takuson2525zt@gmail.com>
2023-07-16 14:10:10 +0900

    Update .config/nvim/lua/plugins/indent-blankline.lua

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.opt.list = true
     vim.opt.listchars:append "eol:↴"
-    vim.cmd [[highlight IndentBlanklineIndent1 guifg=#636363 gui=nocombine]]
+    vim.cmd [[highlight IndentBlanklineIndent1 guifg=#dedede gui=nocombine]]
     require("indent_blankline").setup{
       space_char_blankline = " ",
       show_current_context = true,
